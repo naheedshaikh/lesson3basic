c3530c1f0ffaebec84cb5e055fa34e91c3ab8acf
Naheed Shaikh <naheed.shaikh@dcmail.ca>
2020-02-29 00:26:15 -0500

    Attaching Classes to the objects

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,43 +1,6 @@
 "use strict";
 //IIFE - Immediately Invoked Function Expression
 // mean? -> anonymous self-executing function
-let object = {};
-object.firstProperty = "Some String Property";
-object.secondProperty = 5;
-object.thirdProperty = function(num) 
-{
-    num *= num;
-    return num;
-}
-object.fourthProperty = ['Tom', 45, 'funky dog', 25.3];
-
-let object2 = 
-{
-    "firstProperty": "Some String",
-    "secondProperty": 10,
-    "thirdProperty": [1, 2, 3, 4, "some weird thing"]
-}
-
-let myObjectFunction = function(anObject)
-{
-    anObject.firstProperty = "another String";
-    return anObject;
-}
-
-let myNewObject = myObjectFunction(object2);
-
-let object3 = new Object();
-
-object3.myFunction = new Function();
-object3.myFunction.someValue = 5;
-object3.myFunction.someArray = new Array(20);
-
-Student.prototype.myNewFunction = () => 
-{
-    console.log("hello");
-}
-
-console.log(Student);
 
 let app;
 (function(app){
