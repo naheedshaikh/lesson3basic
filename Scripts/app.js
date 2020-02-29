7794a79755e78f1fc5d8072ca75400cabee048c3
Naheed Shaikh <naheed.shaikh@dcmail.ca>
2020-02-28 22:13:17 -0500

    Adding variable app in class objects

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,12 +1,32 @@
+"use strict";
+//IIFE - Immediately Invoked Function Expression
+// mean? -> anonymous self-executing function
 
-// IIFE - Immediately Invoked Function Expression
-(function(){
+let app;
+(function(app){
+    // Declare Function Variables here...
+    console.log("%cDeclaring Variables", "color: red;")
+
+    /**
+     * Variable initialization in this function
+     *
+     */
 
     function Start()
     {
-        console.log("App Started");
+        console.log("%cInitializing Variables", "color: blue;")
+        Main();
+    }
+
+    /**
+     * Main Program entry point is here
+     *
+     */
+    function Main()
+    {
+        console.log("%cMain Program Body", "color: green;")
     }
 
     window.addEventListener("load", Start);
     
-})();
+})(app || (app = {}));
